39f0f73a6835640f3b5cf88333642f12fea5525b
GabrieleSalati <gabriele.salati5@gmail.com>
2023-09-25 11:49:48 +0200

    Add project files.

---
+++ ClassLibraryPlugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPlugin
+{
+    public class Class1
+    {
+    }
+}
